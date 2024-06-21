8d15c35ecd835e4f1c2ca7fb62ca8fed6797153b
Rharan-Ru <rharanru@gmail.com>
2024-06-20 22:38:36 -0300

    [Rharan] chore: Add initial project files and structure

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { TodoModule } from './modules/todo/todo.module';
+
+@Module({
+  imports: [AuthModule, UserModule, TodoModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
