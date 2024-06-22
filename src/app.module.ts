b72fc6b4566f04b4e51209f2f08e9be4b2792842
Rharan-Ru <rharanru@gmail.com>
2024-06-22 10:09:17 -0300

    [Rharan] feat: Update npm dependencies and add user-specific functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UserModule } from './modules/user/user.module';
 import { TodoModule } from './modules/todo/todo.module';
@@ -8,7 +6,7 @@ import { AppConfigModule } from './config/config.module';
 
 @Module({
   imports: [AppConfigModule, AuthModule, UserModule, TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
