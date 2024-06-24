24668ea768d4da9bfc98f38e1f9db312b813c5a7
Rharan-Ru <rharanru@gmail.com>
2024-06-24 09:27:18 -0300

    feat: Add @types/cors npm dependency for CORS support

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './shared/filters/exceptionsCatch.filter';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,16 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-doc', app, document);
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  app.use(
+    cors({
+      origin: 'https://todo.themonkedesk.online',
+      credentials: true,
+      maxAge: 86400,
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+    }),
+  );
 
   await app.listen(3000);
 }
